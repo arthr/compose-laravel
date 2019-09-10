176f84daf1904f9f3271d7832e58423927bb6a86
Arthur Morais <arthrmrs@gmail.com>
2019-09-10 16:46:39 -0300

    redis tests

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -10,6 +10,7 @@ class RedisController extends Controller
     public function index(Request $request)
     {
         try {
+            \Cache::store('redis')->put('Laradock', 'Awesome', 10);
             $redis = new \Redis();
             dd($redis);
         } catch (Exception $e) {
