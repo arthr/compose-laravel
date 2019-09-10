d55686f2905c62a1b213f09d35f7cb7e455f3bbc
Arthur Morais <arthrmrs@gmail.com>
2019-09-10 16:11:05 -0300

    redisConn

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Redis as RedisConn;
 
 class RedisController extends Controller
 {
     public function index(Request $request)
     {
         try {
-            $redis = Redis::connection();
+            $redis = RedisConn::connection();
             dd($redis);
         } catch (Exception $e) {
             dd($e->getMessage());
