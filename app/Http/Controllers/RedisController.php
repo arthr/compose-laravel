7f712254c7fedd0cf78db53bea241cf8e6683186
Arthur Morais <arthrmrs@gmail.com>
2019-09-10 15:17:38 -0300

    redis connection

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class RedisController extends Controller
 {
     public function index(Request $request)
     {
-        print_r('Redis');
+        try {
+            $redis = Redis::connection('redis', 16379);
+            return response('redis working');
+        } catch (\Predis\Connection\ConnectionException $e) {
+            return reponse('redis connection error');
+        }
     }
 }
