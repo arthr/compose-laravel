cc7e35eae30c22b30c9c7214b410d79d7ff3e73b
Arthur Morais <arthrmrs@gmail.com>
2019-09-10 16:22:26 -0300

    predis added

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class RedisController extends Controller
 {
     public function index(Request $request)
     {
         try {
-            $redis = RedisManager::connection();
+            $redis = Redis::connection();
             dd($redis);
         } catch (Exception $e) {
             dd($e->getMessage());
