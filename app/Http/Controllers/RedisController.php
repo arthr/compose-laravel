fef4d1f6eac20bd43ef9f89335f20cc49f26675f
Arthur Morais <arthrmrs@gmail.com>
2019-09-10 16:42:26 -0300

    redis tests

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 
 class RedisController extends Controller
 {
     public function index(Request $request)
     {
         try {
-            $redis = Redis::connection();
+            $redis = new \Redis();
             dd($redis);
         } catch (Exception $e) {
             dd($e->getMessage());
